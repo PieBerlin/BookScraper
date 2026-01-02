19a83c46aee86fef14ad7aaad9375f99982d24f1
PieBerlin <brightongorova@gmail.com>
2026-01-02 21:14:29 +0300

    first commit

---
+++ bookscraper/bookscraper/settings.py
@@ -0,0 +1,113 @@
+# Scrapy settings for bookscraper project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://docs.scrapy.org/en/latest/topics/settings.html
+#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = "bookscraper"
+
+FEEDS={
+    'booksdata.json':{'format':'json','overwrite':'True'},
+}
+
+SPIDER_MODULES = ["bookscraper.spiders"]
+NEWSPIDER_MODULE = "bookscraper.spiders"
+
+ADDONS = {}
+
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = "bookscraper (+http://www.yourdomain.com)"
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY =False #True
+
+# Concurrency and throttling settings
+#CONCURRENT_REQUESTS = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 1
+DOWNLOAD_DELAY = 1
+
+# Disable cookies (enabled by default)
+#COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+#DEFAULT_REQUEST_HEADERS = {
+#    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+#    "Accept-Language": "en",
+#}
+
+# Enable or disable spider middlewares
+# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+#    "bookscraper.middlewares.BookscraperSpiderMiddleware": 543,
+#}
+
+# Enable or disable downloader middlewares
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+DOWNLOADER_MIDDLEWARES = {
+   "bookscraper.middlewares.BookscraperDownloaderMiddleware": 543,
+   "bookscraper.middlewares.ScrapeOpsFakeUserAgentMiddleware": 400,
+   "bookscraper.middlewares.ScrapeOpsFakeBrowserHeaderAgentMiddleware": 500,
+   'rotating_proxies.middlewares.RotatingProxyMiddleware': 610,
+   'rotating_proxies.middlewares.BanDetectionMiddleware': 620,
+}
+
+#ROTAING_PROXIY_LIST_PATH='bookscraper/proxy_list.txt' 
+
+# Enable or disable extensions
+# See https://docs.scrapy.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    "scrapy.extensions.telnet.TelnetConsole": None,
+#}
+
+# Configure item pipelines
+# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+ITEM_PIPELINES = {
+   "bookscraper.pipelines.BookscraperPipeline": 300,
+   #"bookscraper.pipelines.SaveToMySQLPipeline": 400,
+}
+SCRAPEOPS_API_KEY='8238e78d-8c91-48dc-bbd2-85edf5cba21d'
+# SCRAPEOPS_FAKE_BROWSER_HEADER_ENDPOINT=''
+SCRAPEOPS_FAKE_BROWSER_HEADER_ENABLED=True
+SCRAPEOPS_FAKE_USER_AGENT_ENDPOINT='https://headers.scrapeops.io/v1/user-agents'
+SCRAPEOPS_FAKE_USER_AGENT_ENABLED=True
+SCRAPEOPS_NUM_RESULTS=50
+
+ROTAING_PROXIY_LIST=[
+    '188.121.123.124:1080',
+    '160.153.245.187:2287',
+   '95.216.201.74:1080',
+]
+
+#USER_AGENT='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0'
+
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
+#AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+#AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+#AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = "httpcache"
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
+
+# Set settings whose default value is deprecated to a future-proof value
+FEED_EXPORT_ENCODING = "utf-8"
---
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -0,0 +1,129 @@
+"""import scrapy
+from bookscraper.items import BookItem
+import random
+
+
+class BookspiderSpider(scrapy.Spider):
+    name = "bookspider"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com"]
+
+    custom_settings={
+        'FEEDS':{
+            'booksdata.json':{'format':'json'},
+        }
+    }
+    user_agent_list = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux i686; rv:109.0) Gecko/20100101 Firefox/121.0',
+    'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/121.0',
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_0) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Safari/605.1.15",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (Linux; Android 13; Pixel 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Edg/120.0.0.0 Chrome/120.0.0.0 Safari/537.36",
+    "Opera/9.80 (Windows NT 6.1; U; en) Presto/2.12.388 Version/12.18",
+    "curl/7.88.1",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/120.0.0.0 Safari/537.36",
+]
+
+
+    def parse(self, response):
+        books=response.css("article.product_pod")
+        for book in books:
+            relative_url=book.css('h3 a ::attr(href)').get()
+            if 'catalogue/' in relative_url:
+                book_url='https://books.toscrape.com/'+relative_url
+            else:
+                book_url='https://books.toscrape.com/catalogue/'+relative_url
+            yield response.follow(book_url,callback=self.parse_book_page)
+        
+        next_page=response.css('li.next a ::attr(href)').get()
+        if next_page is not None:
+            if 'catalogue/' in next_page:
+                next_page_url='https://books.toscrape.com/'+next_page
+            else:
+                next_page_url='https://books.toscrape.com/catalogue/'+next_page
+            yield response.follow(next_page_url,callback=self.parse)
+
+    def parse_book_page(self,response):
+        table_rows=response.css("table tr")
+        book_item=BookItem()
+    
+        book_item['url']=response.url 
+        book_item['title']=response.css('.product_main h1::text').get()
+        book_item['upc']=response.css('td ::text').get()
+        book_item['product_type']=table_rows[1].css("td ::text").get()
+        book_item['price_excl_tax']=table_rows[2].css("td ::text").get()
+        book_item['price_incl_tax']=table_rows[3].css("td ::text").get()
+        book_item['tax']=table_rows[4].css("td ::text").get()
+        book_item['availability']=table_rows[5].css("td ::text").get()
+        book_item['num_reviews']=table_rows[6].css("td ::text").get()
+        book_item['stars']=response.css("p.star-rating").attrib['class']
+        book_item['category']=response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get()
+        book_item['description']=response.xpath("//div[@id='product_description']/following-sibling::p/text()").get()
+        book_item['price']=response.css("p.price_color ::text").get()
+            
+        yield book_item
+
+
+""" 
+import scrapy
+from bookscraper.items import BookItem  
+
+class BookspiderSpider(scrapy.Spider):
+    name = 'bookspider'
+    allowed_domains = ['books.toscrape.com']
+    start_urls = ['https://books.toscrape.com/']
+
+    custom_settings = {
+        'FEEDS': {
+            'booksdata.json': {'format': 'json', 'overwrite': True},
+        }
+    }
+
+    def parse(self, response):
+        books = response.css('article.product_pod')
+        for book in books:
+            relative_url = book.css('h3 a ::attr(href)').get()
+
+            if 'catalogue/' in relative_url:
+                book_url = 'https://books.toscrape.com/' + relative_url
+            else:
+                book_url = 'https://books.toscrape.com/catalogue/' + relative_url
+            yield response.follow(book_url, callback=self.parse_book_page)
+
+        next_page = response.css('li.next a ::attr(href)').get()
+        if next_page is not None:
+            if 'catalogue/' in next_page:
+                next_page_url = 'https://books.toscrape.com/' + next_page
+            else:
+                next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
+            yield response.follow(next_page_url, callback=self.parse)
+
+
+    def parse_book_page(self, response):
+
+        table_rows = response.css("table tr")
+        book_item = BookItem()
+
+    
+        book_item['url'] = response.url,
+        book_item['title'] = response.css('.product_main h1::text').get()
+        book_item['upc'] = table_rows[0].css("td ::text").get()
+        book_item['product_type' ] = table_rows[1].css("td ::text").get()
+        book_item['price_excl_tax'] = table_rows[2].css("td ::text").get()
+        book_item['price_incl_tax'] = table_rows[3].css("td ::text").get()
+        book_item['tax'] = table_rows[4].css("td ::text").get(),
+        book_item['availability'] = table_rows[5].css("td ::text").get()
+        book_item['num_reviews']=  table_rows[6].css("td ::text").get()
+        book_item['stars'] = response.css("p.star-rating").attrib['class']
+        book_item['category'] = response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get()
+        book_item['description'] = response.xpath("//div[@id='product_description']/following-sibling::p/text()").get()
+        book_item['price'] = response.css('p.price_color ::text').get()
+    
+        yield book_item
